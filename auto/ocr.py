2cf8026ebe3a7bd2790c005e2ab91747bc66c4bf
GProduct <gaelancherif@gmail.com>
2024-09-25 20:25:23 +0200

    Refactor code to update Tesseract OCR path and add required packages

--- auto/ocr.py
+++ auto/ocr.py
@@ -5,7 +5,7 @@ import os
 import datetime
 
 # Chemin vers l'exécutable Tesseract si nécessaire
-pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"./utils/tesseract/tesseract.exe"
 
 def getOCR(docId):
     path = f"""result/{datetime.datetime.now().strftime('%Y-%m-%d')}/{docId}/"""
